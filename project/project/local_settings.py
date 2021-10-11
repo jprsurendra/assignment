a846eab49df7733fb8c82a53768e52d4fe73e2a9
Surendra Singh Rathore <jpr_surendra@yahoo.co.in>
2021-10-11 18:36:07 +0530

    change git ignore

---
+++ project/project/local_settings.py
@@ -0,0 +1,32 @@
+# import os
+# from django.utils.translation import ugettext_lazy as _
+
+DEBUG = True
+# LOCAL_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+USE_I18N = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_asgmt',
+        'OPTIONS': {
+            # 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER'",
+            'charset': 'utf8'
+        },
+        'USER': 'root',
+        'PASSWORD': 'Jiffy@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+
+
+
+
+
+
+
+
