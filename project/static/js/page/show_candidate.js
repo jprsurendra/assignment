30403dc5716df85baace7714e0e9dc02310948bd
Surendra Singh Rathore <jpr_surendra@yahoo.co.in>
2021-10-12 15:26:33 +0530

    web app

--- project/static/js/page/show_candidate.js
+++ project/static/js/page/show_candidate.js
@@ -2,9 +2,9 @@
 var row_count = '';
 var page = '1'
 
-let booking_filter_data = {};
-let save_curent_booking_table_data = [];
-let save_all_rendered_booking_table_data = {}
+let candidate_filter_data = {};
+let save_curent_candidate_table_data = [];
+let save_all_rendered_candidate_table_data = {}
 
 
 $(document).ready(function () {
@@ -30,7 +30,7 @@ function create_data_table(id, data) {
     },
   ];
 
-  let emptyTableMsg = '<div style="padding:100px 0px"><p class="no_booking_found">You do not have any data</p></div>';
+  let emptyTableMsg = '<div style="padding:100px 0px"><p class="no_candidate_found">You do not have any data</p></div>';
 
 
   let table = $(id).DataTable({
@@ -56,44 +56,43 @@ function render_data_table_body(table_data) {
     }
 
     $("#c_data_table").DataTable().clear().draw();
-    $("#div_showing_bookings").removeClass('d-flex');
-    $("#div_showing_bookings").hide();
+    $("#div_showing_datas").removeClass('d-flex');
+    $("#div_showing_datas").hide();
 
     if (row_data.length > 0) {
         row_data.forEach(function (row) {
-            let action_td = '<a className="btn btn-success btn-sm" href="/candidate/candidate-detail/' + row.id + '">Edit</a>' +
-                            '<a className="btn btn-danger btn-sm" onClick="btn_delete_click(\'' + row.id + '\');"> Delete</a>';
-
             $("#c_data_table").DataTable().row.add({
                   candidate_name: row.candidate_name || "N/A",
                   address: row.address || "N/A",
                   city: row.city.city_name + ' (' + row.city.country.country_name+ ')' || "N/A",
                   owner_info: row.owner_info || "N/A",
                   employee_size: row.employee_size || "N/A",
-                  actions: action_td
+                  id: row.id || "N/A"
             }).draw();
         });
-    let span_of_listing_count_html = common_pagination(table_data, 'booking_');
-    $("#booking_pageination_info").html(span_of_listing_count_html);
-    $("#div_showing_bookings").addClass('d-flex');
-    $("#div_showing_bookings").show();
-
+        let span_of_listing_count_html = common_pagination(table_data, 'candidate_');
+        $("#candidate_pageination_info").html(span_of_listing_count_html);
+        $("#div_showing_datas").addClass('d-flex');
+        $("#div_showing_datas").show();
     }
 }
 
-function fetch_data(page = 1, booking_filter_data = {}) {
-    row_count = $("#no_of_booking_rows").children("option:selected").val();
-    booking_filter_data['page_size']= row_count;
-    booking_filter_data["page"] = page;
+function fetch_data(page = 1, candidate_filter_data = {}) {
+    row_count = $("#no_of_candidate_rows").children("option:selected").val();
+    candidate_filter_data['page_size']= row_count;
+    candidate_filter_data["page"] = page;
     $.ajax({
         method: "GET",
         url: "/candidateapi/candidate/",
         async: false,
-        data: booking_filter_data,
+        data: candidate_filter_data,
         success: function (data) {
-          save_curent_booking_table_data = data;
-          save_all_rendered_booking_table_data[page]= data;
+          save_curent_candidate_table_data = data;
+          save_all_rendered_candidate_table_data[page]= data;
           render_data_table_body(data);
         },
     });
+}
+function candidate_row_count_change() {
+  fetch_data(1, candidate_filter_data)
 }
